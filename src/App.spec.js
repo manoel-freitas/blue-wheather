d4f2a4ce8111e712ce728881e91ddbce07cb6e73
manoel.josefneto <manoel.josefneto@outlook.com>
2019-10-07 21:03:29 -0300

    fix app spec file

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,7 +3,6 @@
 import { shallowMount } from '@vue/test-utils'
 import App from './App'
 
-
 test('App has to be mounted', () => {
   const app = shallowMount(App)
   expect(app.isVueInstance()).toBeTruthy()
